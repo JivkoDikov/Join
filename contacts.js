19d388ab0f5e582ccbac6547730bf4b5bf6cf3b8
Jivko Dikov <145717418+JivkoDikov@users.noreply.github.com>
2023-12-29 00:18:58 +0100

    revise contact.js and contact.css

--- contacts.js
+++ contacts.js
@@ -1,18 +1,28 @@
 let contactDetails =[];
-
-function addNewContact() {
-
-
+save();
+load();
+async function addNewContact() {
+  
    document.getElementById('addnewcontact').classList.add('showOverlay-addNewContactPopUpContainer');
+  // document.getElementById('addnewcontact').classList.add('bodyopacity');
+
+   
+   
+
+
 }
 
 function closeAddNewContact(){
   
   document.getElementById('addnewcontact').classList.remove('showOverlay-addNewContactPopUpContainer');
+  
 
 }
 
 function createContact(){
+  
+  document.getElementById('addnewcontact').classList.remove('showOverlay-addNewContactPopUpContainer');
+
   let name = document.getElementById('name');
   let email = document.getElementById('email');
   let tel = document.getElementById('tel');
@@ -22,9 +32,48 @@ function createContact(){
     "email": email.value,
     "tel": tel.value
   };
-
   contactDetails.push(contactDetail);
   console.log(contactDetails);
-  
+  name.value ='';
+  email.value='';
+  tel.value='';
 
+  for (let i = 0; i < contactDetails.length; i++) {
+    const detail = contactDetails[i];
+    
+  
+  document.getElementById('addcontactContainer').innerHTML += /*html*/`
+  <div id="iconNameEmailContainer"class="iconNameEmailContainer">
+    <div class="iconNameEmail">
+    <div class="firstLastLetter">JD</div>
+    <div class="nameEmail">
+  <div class="name">${detail["name"]}</div>
+  <div class="email">${detail["email"]}</div>
+  </div>
+  </div>
+
+  </div>
+  
+  
+  
+  
+  `;
+  
+ 
+  }
+  
+  
+}
+function save(){
+  let contactDetailsAsText = JSON.stringify(contactDetails);
+  localStorage.setItem("contactDetails", contactDetailsAsText);
+}
+
+function load(){
+  let contactDetailsAsText = localStorage.getItem('contactDetails');
+  if(contactDetailsAsText){
+    contactDetails = JSON.parse(contactDetailsAsText);
+
+  }
+  
 }
