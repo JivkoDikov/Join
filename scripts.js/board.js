12f46bafc021fdafc05b20cf24371dd93df0e40f
FloJung <flo-fj@gmx.at>
2023-12-28 18:18:03 +0100

    first commit for board

---
+++ scripts.js/board.js
@@ -0,0 +1,10 @@
+let cards = [
+    {
+        "id" : 0,
+        "label" : "User Story",
+        "headline" : "Kochwelt Page & Recipe Recommender",
+        "text": "Build start page with recipe recommendation...",
+        "progressBar" : 1,
+        
+    },
+]
