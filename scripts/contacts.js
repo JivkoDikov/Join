476c5dcba4aad68e4c712bef8c0761b919a8ccf5
Jivko Dikov <145717418+JivkoDikov@users.noreply.github.com>
2024-01-19 14:16:36 +0100

    new contacts.js

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,6 +1,28 @@
-let firstLetters = [];
-let lastLetters = [];
-let contactDetails = [];
+let letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let contacts = [ {
+  id : 0,
+  name: "Franz Meier",
+  email: "franzmeier@hotmail.com",
+  tel: "5458938498",
+},
+{
+  id : 1,
+  name: "Sepp Schuster",
+  email: "seppschuster@hotmail.com",
+  tel: "5889523049",
+},
+{
+  id : 2,
+  name: "Max Mustermann",
+  email: "maxmustermann@hotmail.com",
+  tel: "9080983409",
+},
+{
+  id : 3,
+  name: "Anton Hermann",
+  email: "antonhermann@hotmail.com",
+  tel: "485039485"
+}];
 let backgroundColors = [
     { color: "rgb(147, 39, 255)" },
     { color: "rgb(110, 82, 255)" },
@@ -35,150 +57,58 @@ function createContact(event) {
   let name = document.getElementById("name");
   let email = document.getElementById("email");
   let tel = document.getElementById("tel");
-  let contactDetail = {
+  let contact = {
+    id : 5,
     name: name.value,
     email: email.value,
     tel: tel.value,
   };
-  contactDetails.push(contactDetail);
+  contacts.push(contact);
   name.value = "";
   email.value = "";
   tel.value = "";
-  let detail = contactDetails[contactDetails.length - 1];
-  let firstLetter = detail["name"][0].toUpperCase();
-  let lastLetter = detail["name"].split(" ")[1]?.[0]?.toUpperCase() || "";
-  if (!firstLetters.includes(firstLetter)) {
-    firstLetters.push(firstLetter);
-  }
-  if (!lastLetters.includes(lastLetter)) {
-    lastLetters.push(lastLetter);
-  }
-document.getElementById("letterBox").innerHTML = '';
+  
 
 
 
+  
 
-  showContact();
+  showContacts();
 }
 
-function showContact() {
-  let groupedContacts = {};
-  for (let i = 0; i < contactDetails.length; i++) {
-      const detail = contactDetails[i];
-      const firstLetter = detail["name"][0].toUpperCase();
-      if (!groupedContacts[firstLetter]) {
-          groupedContacts[firstLetter] = [];
-      }
-      groupedContacts[firstLetter].push(detail);
-  }
-  for (let letter in groupedContacts) {
-      const contacts = groupedContacts[letter];
-      document.getElementById("letterBox").innerHTML += /*html*/ `
-          <div id="firstLetterContainer" class="firstLetterContainer" onclick="renderContact('${letter}')">${letter}</div>
-          <div class="line"></div>
-          <div id="renderContactDetails${letter}"></div>`;
-      for (let i = 0; i < contacts.length; i++) {
-          const detail = contacts[i];
-          let backgroundColor = backgroundColors[i % backgroundColors.length];
-
-          let lastLetter = detail["name"].split(" ")[1]?.[0]?.toUpperCase() || "";
-
-        document.getElementById(`renderContactDetails${letter}`).innerHTML += /*html*/ `
-            <div onclick="renderContact('${letter}')" id="iconNameEmailContainer" class="iconNameEmailContainer" >
+function showContacts() {
+  let letterBox = document.getElementById("letterBox");
+  letterBox.innerHTML ='';
+  for (let i = 0; i < contacts.length; i++) {
+      const contact = contacts[i];
+      let backgroundColor = backgroundColors[i % backgroundColors.length];
+      letterBox.innerHTML += /*html*/ `
+            <div id="iconNameEmailContainer" class="iconNameEmailContainer" >
                 <div class="iconNameEmail">
                     <div id="firstLastLetter" class="firstLastLetter" style="background-color: ${backgroundColor.color}">
-                        ${letter}${lastLetter}
+                      
                     </div>
                     <div class="nameEmail">
-                        <div class="name">${detail["name"]}</div>
-                        <div class="email">${detail["email"]}</div>
+                        <div class="name">${contact["name"]}</div>
+                        <div class="email">${contact["email"]}</div>
                     </div>
                 </div>
+
             </div>`;
-    }
+  }
+
+  for (let i = 0; i < letters.length; i++) {
+    const letter = letters[i];
+    document.getElementById("letterBox").innerHTML += /*html*/ `
+    <div id="firstLetterContainer" class="firstLetterContainer">${letter}</div>
+  <div class="line"></div>`;
+  }
  
 }
-showContactDetails();
-}
 
-function showContactDetails(){
-  
-  for (let i = 0; i < contactDetails.length; i++) {
-    const detail = contactDetails[i];
-    const firstLetter = detail["name"][0].toUpperCase();
-    const lastLetter = detail["name"].split(" ")[1]?.[0]?.toUpperCase() || "";
-    let backgroundColor = backgroundColors[i % backgroundColors.length];
- 
-    document.getElementById("showContact").innerHTML = /*html*/ `
-  <div id="contactContainerContact" class="contactContainerContact">
-    <div class="contactContainerContactIconName">
-      <div id="contactContainerContactIcon"class="contactContainerContactIcon" style="background-color: ${backgroundColor.color}" >${firstLetter}${lastLetter}</div>
-      <div class="nameEditDelete">
-        <div id="contactContainerContactName"class="contactContainerContactName">${detail["name"]}</div>
-        <div class="editDeleteContainer">
-        <div onclick="editContact(${i})"class="iconEdit">
-        <svg class="svgIcons"width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <mask id="mask0_119188_2072" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-            <rect width="24" height="24" fill="#D9D9D9"/>
-            </mask>
-            <g mask="url(#mask0_119188_2072)">
-            <path d="M5 19H6.4L15.025 10.375L13.625 8.975L5 17.6V19ZM19.3 8.925L15.05 4.725L16.45 3.325C16.8333 2.94167 17.3042 2.75 17.8625 2.75C18.4208 2.75 18.8917 2.94167 19.275 3.325L20.675 4.725C21.0583 5.10833 21.2583 5.57083 21.275 6.1125C21.2917 6.65417 21.1083 7.11667 20.725 7.5L19.3 8.925ZM17.85 10.4L7.25 21H3V16.75L13.6 6.15L17.85 10.4Z" fill="#2A3647"/>
-            </g>
-          </svg>
-          <p>Edit</p>
-        </div>
-      <div onclick="deleteContact(${i})"class="iconDelete">
-      <svg class="svgIcons"width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-           <mask id="mask0_119188_3520" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-           <rect width="24" height="24" fill="#D9D9D9"/>
-          </mask>
-          <g mask="url(#mask0_119188_3520)">
-           <path d="M7 21C6.45 21 5.97917 20.8042 5.5875 20.4125C5.19583 20.0208 5 19.55 5 19V6C4.71667 6 4.47917 5.90417 4.2875 5.7125C4.09583 5.52083 4 5.28333 4 5C4 4.71667 4.09583 4.47917 4.2875 4.2875C4.47917 4.09583 4.71667 4 5 4H9C9 3.71667 9.09583 3.47917 9.2875 3.2875C9.47917 3.09583 9.71667 3 10 3H14C14.2833 3 14.5208 3.09583 14.7125 3.2875C14.9042 3.47917 15 3.71667 15 4H19C19.2833 4 19.5208 4.09583 19.7125 4.2875C19.9042 4.47917 20 4.71667 20 5C20 5.28333 19.9042 5.52083 19.7125 5.7125C19.5208 5.90417 19.2833 6 19 6V19C19 19.55 18.8042 20.0208 18.4125 20.4125C18.0208 20.8042 17.55 21 17 21H7ZM7 6V19H17V6H7ZM9 16C9 16.2833 9.09583 16.5208 9.2875 16.7125C9.47917 16.9042 9.71667 17 10 17C10.2833 17 10.5208 16.9042 10.7125 16.7125C10.9042 16.5208 11 16.2833 11 16V9C11 8.71667 10.9042 8.47917 10.7125 8.2875C10.5208 8.09583 10.2833 8 10 8C9.71667 8 9.47917 8.09583 9.2875 8.2875C9.09583 8.47917 9 8.71667 9 9V16ZM13 16C13 16.2833 13.0958 16.5208 13.2875 16.7125C13.4792 16.9042 13.7167 17 14 17C14.2833 17 14.5208 16.9042 14.7125 16.7125C14.9042 16.5208 15 16.2833 15 16V9C15 8.71667 14.9042 8.47917 14.7125 8.2875C14.5208 8.09583 14.2833 8 14 8C13.7167 8 13.4792 8.09583 13.2875 8.2875C13.0958 8.47917 13 8.71667 13 9V16Z" fill="#2A3647"/>
-           </g>
-           </svg>
-        <p>Delete</p>
-  </div>
-      </div>
-      </div>
-    </div>
-    <div class="contactInfoContainer">
-      <p class="contactInfo">Contact Information</p>
-    </div>
-    <div class="emailPhone">
-      <div class="emailContainer">
-        <p class="emailtext">Email</p>
-        <p id="emailadress"class="emailadress">${detail["email"]}</p>
-      </div>
-      <div class="phoneContainer">
-        <p class="phone">Phone</p>
-        <p id="telnumber"class="tel">${detail["tel"]}</p>
-      </div>
-    </div>
-  </div>
-  `;
-  
 
-}
-save();
-load();
-}
 
-function renderContact(i) {
-  const selectedContact = contactDetails[i];
-  const firstLetter = selectedContact["name"][0].toUpperCase();
-  const lastLetter = selectedContact["name"].split(" ")[1]?.[0]?.toUpperCase() || "";
-  const backgroundColor = backgroundColors[i % backgroundColors.length];
-  document.getElementById("contactContainer").classList.add("backgroundColorContact");
-  document.getElementById('contactContainerContact').classList.add('overlay-contactContainerContact');
-  setTimeout(() => {
-    document.getElementById('contactContainerContact').classList.remove('overlay-contactContainerContact');
-  }, 1000);
-  document.getElementById("contactContainerContactName").innerHTML = selectedContact.name;
-  document.getElementById("emailadress").innerHTML = selectedContact.email;
-  document.getElementById("telnumber").innerHTML = selectedContact.tel;
-  document.getElementById("contactContainerContactIcon").innerHTML = `${firstLetter + lastLetter}`;
-  document.getElementById("contactContainerContactIcon").style.backgroundColor = backgroundColor.color;
-}
+
 
 function deleteContact(index) {
   contactDetails.splice(index, 1);
@@ -205,26 +135,22 @@ function closeEditContact() {
 
 
 function save() {
-  let firstLettersAsText = JSON.stringify(firstLetters);
-  localStorage.setItem("firstLetters", firstLettersAsText);
-  let lastLettersAsText = JSON.stringify(lastLetters);
-  localStorage.setItem("lastLetters", lastLettersAsText);
-  let contactDetailsAsText = JSON.stringify(contactDetails);
-  localStorage.setItem("contactDetails", contactDetailsAsText);
+  let lettersAsText = JSON.stringify(letters);
+  localStorage.setItem("letters", lettersAsText);
+  let contactsAsText = JSON.stringify(contacts);
+  localStorage.setItem("contacts", contactsAsText);
   let backgroundColorsAsText = JSON.stringify(backgroundColors);
   localStorage.setItem("backgroundColors", backgroundColorsAsText);
 }
 
 function load() {
-  let lastLettersAsText = localStorage.getItem('lastLetters');
-  let firstLettersAsText = localStorage.getItem('firstLetters');
-  let contactDetailsAsText = localStorage.getItem('contactDetails');
+  let lettersAsText = localStorage.getItem('letters');
+  let contactsAsText = localStorage.getItem('contacts');
   let backgroundColorsAsText = localStorage.getItem('backgroundColors')
 
-  if (lastLettersAsText && firstLettersAsText && contactDetailsAsText && backgroundColorsAsText) {
-    contactDetails = JSON.parse(contactDetailsAsText);
-    firstLetters = JSON.parse(firstLettersAsText);
-    lastLetters = JSON.parse(lastLettersAsText);
+  if (lettersAsText && contactsAsText && backgroundColorsAsText) {
+    contacts = JSON.parse(contactsAsText);
+    letters = JSON.parse(lettersAsText);
     backgroundColors = JSON.parse(backgroundColorsAsText);
   }
 }
