5300da84305901a43a9e7ccf9382b79d54021b9c
Juri <neizcon@gmail.com>
2024-01-20 19:55:02 +0100

    add currentDate in addTask

--- scripts/addtask.js
+++ scripts/addtask.js
@@ -26,6 +26,17 @@ function inputFrame(id){
     inputField.style.border = "1px solid red";
     required.classList.remove("d-none")
   }
-
-  
 }
+
+function getCurrentDate() {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("enterDate").value = getCurrentDate();
+});
